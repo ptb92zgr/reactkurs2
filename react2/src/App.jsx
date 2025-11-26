ad219f21b687924d9ed7fec2fcb30a340e26891b
Patryk Janicki <patryk.janicki@beefee.co.uk>
2025-11-26 20:01:32 +0100

    usestate without refactor

--- react2/src/App.jsx
+++ react2/src/App.jsx
@@ -1,22 +1,16 @@
+import { useState } from 'react';
+
 function App() {
-  function logMouseLeave() {
-    console.log('on mouse out');
+  const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+  const [isButtonShown, setIsButtonShown] = useState(true);
+
+  function handleClick() {
+    setIsSpoilerShown(true);
+    setIsButtonShown(false);
+    console.log('handleClick');
   }
 
-  const buttonEl = (
-    <button
-      onClick={() => alert('KLIK')}
-      onMouseEnter={() => console.log('on mouse enter')}
-      onMouseOut={logMouseLeave}
-    >
-      Pokaż spoiler
-    </button>
-  );
-
-  // document.querySelector('button').addEventListener('click', () => {
-  //   alert('KLIK');
-  // });
-
+  console.log('App render');
   return (
     <>
       <h1>gwiezdne wojny V</h1>
@@ -25,8 +19,10 @@ function App() {
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, fuga?
       </p>
-      {buttonEl}
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      {isButtonShown && <button onClick={handleClick}>Pokaż spoiler</button>}
+      {isSpoilerShown && (
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      )}
     </>
   );
 }
