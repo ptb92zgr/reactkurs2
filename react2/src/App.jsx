5c1afaec22fa92b70f44a943aa76f3a14a7ad368
Patryk Janicki <patryk.janicki@beefee.co.uk>
2025-12-02 18:05:10 +0100

    logic grouped - refactor

--- react2/src/App.jsx
+++ react2/src/App.jsx
@@ -1,63 +1,13 @@
-import { useState } from 'react';
-
-const initialState = {
-  isSpoilerShown: false,
-  isWarningShown: true,
-  numberOfLikes: 50,
-};
+import { LikesCounter } from './components/LikesCounter';
+import { Plot } from './components/Plot';
 
 function App() {
-  const [state, setState] = useState(initialState);
-
-  function handleShowSpoilerClick() {
-    setState((prevState) => ({
-      ...prevState,
-      isWarningShown: false,
-      isSpoilerShown: true,
-    }));
-  }
-
-  function handleCloseWarningClick() {
-    setState((prevState) => ({ ...prevState, isWarningShown: false }));
-  }
-
-  function handleLikeButtonClick() {
-    setState((prevState) => ({
-      ...prevState,
-      numberOfLikes: prevState.numberOfLikes + 1,
-    }));
-  }
-
-  function handleLoveButtonClick() {
-    setState((prevState) => ({
-      ...prevState,
-      numberOfLikes: prevState.numberOfLikes + 3,
-    }));
-  }
-
-  console.log('App render');
   return (
     <>
-      <h1>gwiezdne wojny V</h1>
-      <h2>Data produkcji: 1988</h2>
-      <h2>Liczba polubień:{state.numberOfLikes}</h2>
-      <button onClick={handleLikeButtonClick}>Lubię to!</button>
-      <button onClick={handleLoveButtonClick}>Kocham to!</button>
-      <h2>Fabuła</h2>
-      {state.isWarningShown && (
-        <p>
-          Uwaga opis fabuły zawiera spoiler!
-          <button onClick={handleCloseWarningClick}>X</button>
-        </p>
-      )}
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, fuga?
-      </p>
-      {state.isSpoilerShown ? (
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-      ) : (
-        <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button>
-      )}
+      <h1>Gwiezdne wojny V</h1>
+      <h2>Rok produkcji: 1980</h2>
+      <LikesCounter />
+      <Plot />
     </>
   );
 }
---
+++ react2/src/components/Plot.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export function Plot() {
+  const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+  const [isWarningShown, setIsWarningShown] = useState(true);
+
+  function handleShowSpoilerClick() {
+    setIsSpoilerShown(true);
+    setIsWarningShown(false);
+  }
+
+  function handleCloseWarningClick() {
+    setIsWarningShown(false);
+  }
+
+  return (
+    <>
+      <h2>Fabuła</h2>
+      {isWarningShown && (
+        <p>
+          Uwaga! Opis fabuły zawiera spoilery!
+          <button onClick={handleCloseWarningClick}>X</button>
+        </p>
+      )}
+      <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
+      {isSpoilerShown ? (
+        <p>Vader okazuje się być ojcem Luka.</p>
+      ) : (
+        <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button>
+      )}
+    </>
+  );
+}
